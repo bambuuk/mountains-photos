4344f42c36a580cb6c561e1f91a9412b8ddbb0be
Ivan <vanyok.korobka@ukr.net>
2023-10-04 13:39:42 +0300

    feat/modificate Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
-import {FC} from 'react';
+import { FC, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import { Container } from './Container';
+import { FiSun, FiMoon } from 'react-icons/fi';
 
 const HeaderEl = styled.header`
   background-color: var(--colors-ui-base);
@@ -9,14 +10,61 @@ const HeaderEl = styled.header`
 
 const Wrapper = styled.div`
   padding: 10px 0;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Logo = styled.div`
+  font-size: var(--fs-lg);
+  font-weight: var(--fw-bold);
+  text-transform: capitalize;
+  user-select: none;
+`;
+
+const ThemeSwitcherWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 5px;
+  cursor: pointer;
+  color: var(--colors-text);
+  transition: color 0.2s ease-in-out;
+  user-select: none;
+  &:hover svg {
+    color: var(--colors-hover-effect);
+  }
+`;
+
+const ThemeSwitcher = styled.div`
+  margin-top: 3px;
+  font-size: var(--fs-xxl);
+  svg {
+    transition: color 0.2s ease-in-out;
+  }
+`;
+
+const ThemeSwitcherText = styled.div`
+  font-size: var(--fs-lg);
+  text-transform: capitalize;
 `;
 
 const Header: FC = () => {
+  const [theme, setTheme] = useState('light');
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+  }, [theme])
   return (
     <HeaderEl>
       <Container>
         <Wrapper>
-          header
+          <Logo>Photo list</Logo>
+          <ThemeSwitcherWrapper onClick={() => setTheme((state) => state === 'dark' ? 'light' : 'dark')}>
+            <ThemeSwitcher>
+              {theme === 'dark' ? <FiMoon /> : <FiSun />}
+            </ThemeSwitcher>
+            <ThemeSwitcherText>{`${theme} theme`}</ThemeSwitcherText>
+          </ThemeSwitcherWrapper>
         </Wrapper>
       </Container>
     </HeaderEl>
