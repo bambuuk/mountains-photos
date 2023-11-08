12813d957ba1e158de24cffa0ea6554f1e96e431
Ivan <vanyok.korobka@ukr.net>
2023-11-08 22:18:42 +0200

    create data base info

---
+++ src/data/data.ts
@@ -0,0 +1,304 @@
+const arrInfo = [
+  {
+    id: '1',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain1.jpg?raw=true',
+    comments: [
+      {
+        date: '04/10/2023',
+        text: 'mountain'
+      }
+    ],
+  },
+  {
+    id: '2',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain2.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '3',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain3.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '4',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain4.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '5',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain5.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '6',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain6.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '7',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain7.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '8',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain8.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '9',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain9.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '10',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain10.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '11',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain11.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '12',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain12.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '13',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain13.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '14',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain14.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '15',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain15.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '16',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain16.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '17',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain17.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '18',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain18.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '19',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain19.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '20',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain20.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '21',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain21.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '22',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain22.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '23',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain23.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '24',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain24.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '25',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain25.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '26',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain26.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '27',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain27.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '28',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain28.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '29',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain29.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+  {
+    id: '30',
+    img: 'https://github.com/bambuuk/photos-list/blob/main/src/assets/mountain30.jpg?raw=true',
+    comments: [
+      {
+        date: '',
+        text: ''
+      }
+    ],
+  },
+];
+
+console.log(JSON.stringify(arrInfo));
